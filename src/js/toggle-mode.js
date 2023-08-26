26031cd9472aee6a59d1ab6f0226b0b427041631
JP Sampaio <108679561+jp-sampaio@users.noreply.github.com>
2023-08-25 21:47:21 -0300

    Boa parte já concluída

---
+++ src/js/toggle-mode.js
@@ -0,0 +1,14 @@
+let darkMode = true;
+const buttonToggle = document.querySelector('.toggle-mode');
+
+buttonToggle.addEventListener("click", event => {
+  // adiciona uma class="light" na tag html, elemento raíz.
+  document.documentElement.classList.toggle("light");
+
+  const mode = darkMode ? "light" : "dark";
+  event.currentTarget.
+  querySelector("span").textContent = `${mode} mode ativado!`;
+
+  // negando o valor atual
+  darkMode = !darkMode;
+}) 
